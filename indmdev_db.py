02ea87df66c11adfeddbba291601f845008d4406
indmdev <147909393+indmdev@users.noreply.github.com>
2025-07-18 19:53:25 +0100

    Update indmdev_db.py

--- indmdev_db.py
+++ indmdev_db.py
@@ -4,16 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CREATE TABLE IF NOT EXISTS alerts (
-    id SERIAL PRIMARY KEY,
-    symbol VARCHAR(20),
-    signal_type VARCHAR(10),
-    entry DECIMAL,
-    stop_loss DECIMAL,
-    take_profit DECIMAL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-
 def connect_db():
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST'),
@@ -23,6 +13,22 @@ def connect_db():
         port=os.getenv('DATABASE_PORT')
     )
     return conn
+
+
+def log_alert(symbol, signal_type, entry, sl, tp, timestamp):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS alerts (
+    id SERIAL PRIMARY KEY,
+    symbol VARCHAR(20),
+    signal_type VARCHAR(10),
+    entry DECIMAL,
+    stop_loss DECIMAL,
+    take_profit DECIMAL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );""")
+
 
 def log_alert(symbol, signal_type, entry, sl, tp, timestamp):
     conn = connect_db()
