e8bf2345f19bd8996ecdb4d1c261c289562e71ba
indmdev <147909393+indmdev@users.noreply.github.com>
2025-07-18 19:45:17 +0100

    Update indmdev_db.py

--- indmdev_db.py
+++ indmdev_db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CREATE TABLE IF NOT EXISITS alerts (
+CREATE TABLE IF NOT EXISTS alerts (
     id SERIAL PRIMARY KEY,
     symbol VARCHAR(20),
     signal_type VARCHAR(10),
